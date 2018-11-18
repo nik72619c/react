1b41ce82f0c731acf48d9fd1746bceb81ebbe968
nik72619c <nikhilsharmarockstar21@gmail.com>
2018-11-18 23:53:19 +0530

    working with react-beautiful-dnd

--- react-beautiful-dnd/src/App.js
+++ react-beautiful-dnd/src/App.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { DragDropContext,Droppable,Draggable} from 'react-beautiful-dnd';
 
 class App extends Component {
+
+  onDragEnd(){
+
+  }
   render() {
     return (
+      <DragDropContext onDragEnd={this.onDragEnd.bind(this)}>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Droppable droppableId={1}>
+   {
+     (provided)=>{
+       return <div {...provided.droppableProps} innerRef={provided.innerRef}>
+           <Draggable draggableId={1} index={1}>
+             {
+               (provided)=>{
+                 return  <div style={{
+                  border: '1px solid black',
+                  padding: '10px' ,marginBottom: '3px'
+                }} {...provided.draggableProps} {...provided.dragHandleProps} innerRef={provided.innerRef}>list item 1</div>
+               }
+             }
+           </Draggable>
+
+            <Draggable draggableId={2} index={2} {...provided.draggableProps} {...provided.dragHandleProps} innerRef={provided.innerRef}>
+             {
+               (provided)=>{
+                 return  <div style={{
+                  border: '1px solid black',
+                  padding: '10px' ,marginBottom: '3px'
+                }} {...provided.draggableProps} {...provided.dragHandleProps} innerRef={provided.innerRef}>list item 2</div>
+               }
+             }
+           </Draggable>
+
+            <Draggable draggableId={3} index={3}>
+             {
+               (provided)=>{
+                 return  <div style={{
+                  border: '1px solid black',
+                  padding: '10px' ,marginBottom: '3px'
+                }} {...provided.draggableProps} {...provided.dragHandleProps} innerRef={provided.innerRef}>list item 3</div>
+               }
+             }
+           </Draggable>
+
+ 
+      {provided.placeholder}
+       </div>
+     }
+   }
+      </Droppable>
       </div>
+      </DragDropContext>
     );
   }
 }
