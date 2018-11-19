2e4c7526a267a62afe6ee2983fdfdbbb5965b945
nik72619c <nikhilsharmarockstar21@gmail.com>
2018-11-19 13:32:06 +0530

    added the drag and drop feature

--- react-beautiful-dnd/src/App.js
+++ react-beautiful-dnd/src/App.js
@@ -5,7 +5,24 @@ import { DragDropContext,Droppable,Draggable} from 'react-beautiful-dnd';
 
 class App extends Component {
 
-  onDragEnd(){
+  onDragEnd(result){
+    console.log('onDragEnd function called..');
+    console.log(result);
+
+    // if (!result.destination) {
+    //   return;
+    // }
+
+    // const items = reorder(
+    //   this.state.items,
+    //   result.source.index,
+    //   result.destination.index
+    // );
+
+    // this.setState({
+    //   items,
+    // });
+
 
   }
   render() {
@@ -15,14 +32,11 @@ class App extends Component {
       <Droppable droppableId={1}>
    {
      (provided)=>{
-       return <div {...provided.droppableProps} innerRef={provided.innerRef}>
+       return <div {...provided.droppableProps} ref={provided.innerRef}>
            <Draggable draggableId={1} index={1}>
              {
                (provided)=>{
-                 return  <div style={{
-                  border: '1px solid black',
-                  padding: '10px' ,marginBottom: '3px'
-                }} {...provided.draggableProps} {...provided.dragHandleProps} innerRef={provided.innerRef}>list item 1</div>
+                 return  <div className="listitem" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>list item 1</div>
                }
              }
            </Draggable>
@@ -30,10 +44,7 @@ class App extends Component {
             <Draggable draggableId={2} index={2} {...provided.draggableProps} {...provided.dragHandleProps} innerRef={provided.innerRef}>
              {
                (provided)=>{
-                 return  <div style={{
-                  border: '1px solid black',
-                  padding: '10px' ,marginBottom: '3px'
-                }} {...provided.draggableProps} {...provided.dragHandleProps} innerRef={provided.innerRef}>list item 2</div>
+                 return  <div className="listitem" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>list item 2</div>
                }
              }
            </Draggable>
@@ -41,10 +52,7 @@ class App extends Component {
             <Draggable draggableId={3} index={3}>
              {
                (provided)=>{
-                 return  <div style={{
-                  border: '1px solid black',
-                  padding: '10px' ,marginBottom: '3px'
-                }} {...provided.draggableProps} {...provided.dragHandleProps} innerRef={provided.innerRef}>list item 3</div>
+                 return  <div className="listitem" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>list item 3</div>
                }
              }
            </Draggable>
