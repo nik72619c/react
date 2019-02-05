c5769b15ac09585ce800bd19d62f1b809b6babad
nik72619c <nikhilsharmarockstar21@gmail.com>
2019-02-05 16:26:43 +0530

    INIT

---
+++ react-universal/src/pages/homePageComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Home extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>
+                    my home page
+                </h1>
+                <p>content</p>
+            </div>
+        )
+    }
+}
+
+export default Home;
--- react_ssr/server/index.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-const PORT=3000;
-import serverRenderer from './middleware/renderer';
-const app=express();
-const router=express.Router();
