389295099d0d9a5f4d6976350a1f6e46d165203b
nik72619c <nikhilsharmarockstar21@gmail.com>
2019-03-22 22:53:42 +0530

    snapshot testing

--- testing-with-jest/src/__tests__/demo.test.js
+++ testing-with-jest/src/__tests__/demo.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {render,cleanup} from 'react-testing-library';
-import renderer from 'react-test-renderer';
+import {render,Simulate,cleanup} from 'react-testing-library';
+import App from '../App';
 
-test('testing that jest runs correctly', ()=>{
-    expect(2+2).toBe(4);
-});
+test('check if app renders correclty',()=>{
+    let component=render(<App/>);
+   let el= component.getByText('Learn React');
+   expect(el).toMatchSnapshot();
+})
